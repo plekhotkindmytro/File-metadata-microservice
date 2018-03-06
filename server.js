6c5b98602a82fb43d4f2e9a0cb3775ef21cde3fc
Glitch (file-metadata-plekhotkin) <none>
2018-03-06 12:23:28 +0000

    🌪🕰 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/upload", upload.single('file'), function (request, response, next) {
+  console.log("Uploaded");
+  response.send({size: request.file.size});
+});
+
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
